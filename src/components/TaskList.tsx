424479a5ca11bea27750a5456643b74217efe8a2
Renato Hideki Osaka <renato.osaka@hotmail.com>
2021-03-10 10:21:10 -0300

    feat: challange 01

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,25 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (newTaskTitle === '') {
+      return
+    }
+    
+    const task: Task = {
+      id: Math.floor(Math.random() * 100 + 1),
+      title: newTaskTitle,
+      isComplete: false
+    }
+
+    setTasks(state => [...state, task ])
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    setTasks(state => state.map(task => task.id === id ? { ...task, isComplete: !task.isComplete }: task))
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    setTasks(state => state.filter(task => task.id !== id))
   }
 
   return (
